43462f6feec884b3fd4d549971ca119eb95ec064
Bill Rawlinson <bill@rawlinson.us>
2019-03-26 21:09:31 -0400

    some work toward gettig the game working

--- farkle.js
+++ farkle.js
@@ -9,10 +9,33 @@ class Farkle {
         for(var i = 1; i<=this._dieCount; i++){
             this.dice[i-1] = new Die(this._dieSides);
         }
+
+        this.currentPlayer = null;
+        this.currentPlayerIndex = 0;
+
+        this.drawDice();
+ 
+    }
+
+    startGame(){
+        this.currentPlayer = this.players[this.currentPlayerIndex];
+        this.currentPlayer.giveDice(this.dice);
+    }
+
+    endTurn(){
+        var firstPlayer = 0;
+        var lastPlayer = this.players.length -1;
+        this.currentPlayerIndex = this.currentPlayerIndex === lastPlayer ? firstPlayer : this.currentPlayerIndex++;
+        this.currentPlayer = this.players[this.currentPlayerIndex];
+    }
+
+    roll(){
+        this.currentPlayer.rollDice();
+        this.drawDice();
     }
 
     addPlayer(playerName){
-        this.players.push(playerName);
+        this.players.push(new Player(playerName));
     }
 
     drawGame(){
@@ -21,16 +44,30 @@ class Farkle {
         this.players.forEach((player)=>{
             this.drawPlayer(player);
         });
-        // loop over the players
-        // display them on the screen
-        // display their score along side their names
-        // draw some kind of a dice interface for viewing current dice status
-    }
+   }
 
     drawPlayer(player){
         var p = document.createElement('div');
-        p.innerText = player;
+        p.innerText = player.name;
         document.querySelector('#board').appendChild(p);
+    }
+
+    drawDice(){
+        var div = document.querySelector('#dice');
+        div.innerHTML = '';
+        var index = 0;
+        this.dice.forEach((d)=>{
+            var wrapper = document.createElement('div');
+            wrapper.setAttribute('id','die'+index+'wrapper');
+            wrapper.className = 'col-sm-2 btn btn-danger';
+
+            var span = document.createElement('span');
+            span.innerText=d.value;
+
+            wrapper.appendChild(span);
+            div.appendChild(wrapper);
+        });
+
     }
     
 }
--- models.js
+++ models.js
@@ -1,26 +1,54 @@
 class Die{
     constructor(sides){
         this._sides = sides;
+        this.reset();
     }
 
     roll(){
-        return Math.ceil(Math.random() * this._sides);
+        this.value = Math.ceil(Math.random() * this._sides);
+        return this.value;
+    }
+    reset(){
+        this.value = '-';
+        this.isLocked = 0;
     }
 }
 
 class Player{
     constructor(name){
         this.name = name;
+        this.initialize();
+    }
+
+    initialize(){
+        this.dice = [];
         this.totalScore = 0;
         this.currentTurnScore = 0;
         this.isEndGameStarter = false;
-
-        this.heldDice = [];
         
+        this.heldDice = []; // history of dice kept per roll.
+        this.tempBank = []; // values that can't be picked back up because you've finished the six dice and kept rolling
+    }
+
+    giveDice(dice){
+        this.dice = dice;
     }
 
     rollDice(dice){
+        this.dice.forEach((d)=>{
+            if(!d.isLocked){
+                d.roll();
+            }
+        });
+    }
 
+    continueTurn(){
+        // player kept all six dice and is picking them all up
+        this.tempBank.push(this.heldDice);
+        this.heldDice = [];
+        this.dice.forEach((d)=>{
+            d.reset();
+        });
     }
 }
 
